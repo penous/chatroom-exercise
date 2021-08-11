083914af82c715711979a02bf0c87dfeefce72a9
Tom Dirinck <tom.dirinck@gmail.com>
2021-08-11 19:19:00 +0200

    Added socket io client to react.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io('http://localhost:8080');
 
 function App() {
+  useEffect(() => {
+    // socket.on('connect', () => {
+    //   console.log('hello');
+    // });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
